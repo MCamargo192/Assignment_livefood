6b05b5e421bb3af412e66185463540f86e536281
Marcelo Camargo <macaixeta-camargo@myseneca.ca>
2020-10-20 13:06:01 -0400

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 * of this assignment has been copied manually or electronically from any other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Marcelo Augusto Caixeta Camargo Student ID: 143739191 Date: October 21th, 2020.
+* Name: Marcelo Augusto Caixeta Camargo Student ID: 143739191 Date: October 20th, 2020.
 *
 * Online (Heroku, https://...) Link: https://git.heroku.com/livefood.git
 *
